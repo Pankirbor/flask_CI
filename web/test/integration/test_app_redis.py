61b0bf9498a9bf9ca2eb5b33cf2276e5aff67145
Кирилл Панов <lliril_05@list.ru>
2024-04-23 21:00:02 +0300

    Reorganized the project structure. Preparing for deployment via docker-compose

--- web/test/integration/test_app_redis.py
+++ web/test/integration/test_app_redis.py
@@ -1,9 +1,12 @@
 import pytest
+
+from flask.testing import FlaskClient
+from redis import Redis
 from werkzeug import Response
 
 
 @pytest.mark.timeout(1.5)
-def test_should_update_redis(redis_client, http_client):
+def test_should_update_redis(redis_client: Redis, http_client: FlaskClient):
     redis_client.set("page_views", 4)
     response: Response = http_client.get("/")
 
